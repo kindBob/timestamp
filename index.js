5d5537171afbe150c4a342b077379fae99f729f9
kindBob <asdyrtveflmh@gmail.com>
2023-10-16 18:49:23 +0200

    last commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,16 +5,37 @@ app.use(cors());
 
 app.use(express.static("public"));
 
+app.get("/api", (req, res) => {
+    res.json({
+        unix: new Date().getTime(),
+        utc: new Date().toUTCString(),
+    });
+});
+
 app.get("/api/:date", (req, res) => {
+    if (!req.params.date) {
+        res.json({
+            unix: Date.now(),
+            utc: new Date().toUTCString(),
+        });
+    }
+
     const dateInput = req.params.date;
     let date = new Date(dateInput);
 
     if (date.toString() == "Invalid Date") {
-        date = new Date(parseInt(dateInput) * 1000);
+        date = new Date(parseInt(dateInput));
+
+        if (date.toString() == "Invalid Date") {
+            res.json({ error: "Invalid Date" });
+
+            return;
+        }
     }
     res.json({ unix: Date.parse(date), utc: date.toUTCString() });
 });
 
+// listen for requests :)
 const listener = app.listen(3000, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
